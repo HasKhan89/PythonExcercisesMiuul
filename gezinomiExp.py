bbd9d27041c2a11c737e29c809348dae41b25b6b
Hasan Kahraman <hasankahraman89@hotmail.com>
2026-01-19 15:57:35 +0300

    #GezinomiExpDone

--- gezinomiExp.py
+++ gezinomiExp.py
@@ -60,3 +60,34 @@ labels = ["Last Minuters", "Potantial Planners", "Planners", "Early Bookers" ]
 
 df["EB_Score"] = pd.cut(df["SaleCheckInDayDiff"], bins, labels=labels)
 df.head(50).to_excel("./DATA/eb_scores.xlsx",index=False)
+
+#Gorev 3.1
+df.groupby(["SaleCityName", "ConceptName" , "EB_Score"]).agg({"Price": ["mean" , "count"]})
+df.groupby(["SaleCityName", "ConceptName" , "Seasons"]).agg({"Price": ["mean" , "count"]})
+df.groupby(["SaleCityName", "ConceptName" , "CInDay"]).agg({"Price": ["mean" , "count"]})
+
+#Gorev 4.1
+agg_df = df.groupby(["SaleCityName" , "ConceptName" , "Seasons"]).agg({"Price": "mean"}).sort_values("Price", ascending=False)
+agg_df.head(20)
+
+#Gorev 5
+agg_df.reset_index(inplace=True)
+agg_df.head(20)
+
+
+#Gorev 6
+agg_df["sales_level_based"] = agg_df[["SaleCityName" , "ConceptName" , "Seasons"]].agg(lambda x: "_" .join(x).upper(),axis=1)
+
+#Gorev 7
+agg_df["SEGMENT"]=pd.qcut(agg_df["Price"], 4, labels=["D", "C" , "B", "A"])
+agg_df.head(30)
+agg_df.groupby(["SEGMENT"]).agg({"Price": ["mean" , "max" , "sum"]})
+
+#Gorev 8
+agg_df.sort_values("Price")
+new_user = "ANTALYA_HERŞEY DAHİL_HİGH"
+
+agg_df[agg_df["sales_level_based"] == new_user]
+
+#Gorev 8.1
+#Girne Yarım pansiyon high seviyesi kullanıcı B segmentinde yer almaktadır.
